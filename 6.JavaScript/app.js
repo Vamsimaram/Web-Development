192dda273b0948afa35607b916987b638cef4ee6
M.Vamsi Krishna <79745506+Vamsimaram@users.noreply.github.com>
2024-03-06 23:29:41 -0500

    Add files via upload

---
+++ 6.JavaScript/app.js
@@ -0,0 +1,34 @@
+console.log("First js file");
+console.log(2614);
+console.log(1+1);
+let a=120;
+let b=40;
+console.log(`Total price is ${a+b} rupees`);
+
+//arithmetic opr
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//unary opr
+console.log(a++);
+console.log(++a);
+
+//assignment opr
+console.log(a+=10);
+console.log(a-=10);
+console.log(a*=10);
+console.log(a/=10);
+
+console.log('a'>'A') //unicode of a is 61 and A is 41
+
+let str="apple";
+if(str[0]==='a' && str.length>3){
+    console.log("Good String");
+}
+else{
+    console.log("Bad String");
+}
+
